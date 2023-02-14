3ea1caa506b6d500c3ffe22284caacc82323bb4d
aurajannahya08@gmail.com <aurajanahya08@gmail.com>
2023-02-14 08:55:32 +0700

    ngebenerin icon dan background

--- application/views/template/login.php
+++ application/views/template/login.php
@@ -15,7 +15,7 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="<?= base_url('assets/template') ?>/dist/css/adminlte.min.css">
 </head>
-<section class="vh-80" style="background-image: url('../../../assets/img/background1.jpg')">
+<section class="vh-80" style="background-image: url('../../../assets/img/obat1.png')">
 <body class="hold-transition login-page">
 <div class="login-box">
   <!-- /.login-logo -->
--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -113,8 +113,8 @@
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-      <img src="<?= base_url('assets/template/') ?>dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-" style="opacity: .8">
-      <span class="brand-text font-weight-light">Aplikasi SOP</span>
+      <img src="<?= base_url('assets/template/') ?>dist/img/BE.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-" style="opacity: .8">
+      <span class="brand-text font-weight-light">APLIKASI SOP</span>
     </a>
 
     <!-- Sidebar -->
@@ -122,10 +122,10 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="<?= base_url('assets/template/') ?>dist/img/admin2.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="<?= base_url('assets/template/') ?>dist/img/admin.png" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">IT Admin Aura </a>
+          <a href="" class="d-block">IT Admin Aura </a>
         </div>
       </div>
 
